14a52fb230c6cfd5028951a092e95d212f0c4d20
deshudiosh <deshudiosh@gmail.com>
2022-07-29 02:59:53 +0200

    fancybox

---
+++ projects/orbicular/orbicular.js
@@ -0,0 +1,19 @@
+Fancybox.bind('[data-fancybox]', {
+    animated: false,
+    showClass: false,
+    hideClass: false,
+  
+    // dragToClose: false,
+  
+    closeButton: "top",
+  
+    Thumbs: false,
+    Toolbar: false,
+  
+    Image: {
+      zoom: false,
+      // click: false,
+      wheel: false,
+      // fit: "contain-w",
+    }
+});
