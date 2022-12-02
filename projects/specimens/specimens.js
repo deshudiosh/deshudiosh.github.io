85640c06681823f036491d6df353708a3163d261
deshudiosh <deshudiosh@gmail.com>
2022-12-03 00:28:32 +0100

    specimens 1

---
+++ projects/specimens/specimens.js
@@ -0,0 +1,21 @@
+Fancybox.bind('[data-fancybox]', {
+    animated: false,
+    showClass: false,
+    hideClass: false,
+  
+    // dragToClose: false,
+  
+    closeButton: "top",
+  
+    Thumbs: false,
+    Toolbar: false,
+
+    infinite: false,
+  
+    Image: {
+      zoom: false,
+      // click: false,
+      // wheel: false,
+      // fit: "contain-w",
+    }
+});
